dcee446ef0fc733ce1352ae186d0643f765530f4
Zeeshan Asgar <asgarzeeshan@gmail.com>
2020-11-30 19:36:21 +0000

    polls app added

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+    class Meta:
+        verbose_name = "Choices"
+        verbose_name_plural = "Choices"
